c0307c706448f89307ad37386a518c86b0f2d472
Ibrahim Ashraf <xxibrahimashrafxx@gmail.com>
2025-09-20 09:03:26 +0300

    feat: implement lazy loading for routes and add Loader component for improved user experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,32 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Wishlist from "./pages/Wishlist";
-import Cart from "./pages/Cart";
-import ProductDetails from "./pages/ProductDetails";
-import CategoryDetails from "./pages/CategoryDetails";
-import SearchProducts from "./pages/SearchProducts";
+import { lazy, Suspense } from "react";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Loader from "./ui/Loader";
+
+const Home = lazy(() => import("./pages/Home"));
+const Wishlist = lazy(() => import("./pages/Wishlist"));
+const Cart = lazy(() => import("./pages/Cart"));
+const ProductDetails = lazy(() => import("./pages/ProductDetails"));
+const CategoryDetails = lazy(() => import("./pages/CategoryDetails"));
+const SearchProducts = lazy(() => import("./pages/SearchProducts"));
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/wishlist" element={<Wishlist />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/products/:productId" element={<ProductDetails />} />
-          <Route path="/categroy/:category" element={<CategoryDetails />} />
-          <Route path="/search" element={<SearchProducts />} />
-        </Route>
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/wishlist" element={<Wishlist />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/products/:productId" element={<ProductDetails />} />
+            <Route path="/categroy/:category" element={<CategoryDetails />} />
+            <Route path="/search" element={<SearchProducts />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
---
+++ src/ui/Loader.jsx
@@ -0,0 +1,9 @@
+function Loader() {
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-400/30 backdrop-blur-sm dark:bg-gray-900/30">
+      <div className="loader"></div>
+    </div>
+  );
+}
+
+export default Loader;

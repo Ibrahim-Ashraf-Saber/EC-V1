512c2afe3685394b0a0b2c99b5ed5ea6f01fc68b
Ibrahim Ashraf <xxibrahimashrafxx@gmail.com>
2025-09-20 23:17:40 +0300

    feat: add location-based visibility toggle for SearchBox in Navbar

--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -2,10 +2,17 @@ import Logo from "./Logo";
 import NavIcons from "./NavIcons";
 import SearchBox from "../features/search/SearchBox";
 import { HiOutlineSearch } from "react-icons/hi";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 function Navbar() {
   const [show, setShow] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    setShow(false);
+  }, [location]);
+
   return (
     <nav className="fixed z-30 flex w-full items-center justify-between gap-5 border-b border-gray-100 bg-white px-5 py-5 shadow-sm md:px-20 dark:border-gray-700 dark:bg-gray-900">
       <Logo />
